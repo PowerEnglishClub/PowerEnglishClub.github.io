af7760d6896ea0692be5104ea22763c54be40fe4
OroGeek <ahmia.ahmed@gmail.com>
2023-03-25 15:18:44 +0100

    add slide effect

--- js/script.js
+++ js/script.js
@@ -2,10 +2,83 @@ const menu = document.querySelector("header ul");
 const left = document.getElementById("left-btn");
 const right = document.getElementById("right-btn");
 const menuIcon = document.getElementById("menu-icon");
+const improving = document.querySelector(".improving .content");
 
 const slides = ["language", "listening", "speaking", "reading", "writing"];
 let current = "language";
 let slide = 0;
+
+let mouseX,
+  initialX = 0;
+let mouseY,
+  initialY = 0;
+let isSwiped = false;
+let deviceType;
+
+let events = {
+  mouse: {
+    down: "mousedown",
+    move: "mousemove",
+    up: "mouseup",
+  },
+  touch: {
+    down: "touchstart",
+    move: "touchmove",
+    up: "touchend",
+  },
+};
+
+const isTouchDevice = () => {
+  try {
+    document.createEvent("TouchEvent");
+    deviceType = "touch";
+    return true;
+  } catch (e) {
+    deviceType = "mouse";
+    return false;
+  }
+};
+
+let rectLeft = improving.getBoundingClientRect().left;
+let rectTop = improving.getBoundingClientRect().top;
+
+const getXY = (e) => {
+  mouseX = (!isTouchDevice() ? e.pageX : e.touches[0].pageX) - rectLeft;
+  mouseY = (!isTouchDevice() ? e.pageY : e.touches[0].pageY) - rectTop;
+};
+
+isTouchDevice();
+
+improving.addEventListener(events[deviceType].down, (event) => {
+  isSwiped = true;
+  getXY(event);
+  initialX = mouseX;
+  initialY = mouseY;
+});
+
+improving.addEventListener(events[deviceType].move, (event) => {
+  if (!isTouchDevice()) {
+    event.preventDefault();
+  }
+
+  if (isSwiped) {
+    getXY(event);
+    let diffX = mouseX - initialX;
+    let diffY = mouseY - initialY;
+
+    if (Math.abs(diffY) < Math.abs(diffX)) {
+      diffX > 0 ? ChangeSlide(-1) : ChangeSlide(1);
+    }
+  }
+});
+
+improving.addEventListener(events[deviceType].up, () => {
+  isSwiped = false;
+});
+
+improving.addEventListener("mouseleave", () => {
+  isSwiped = false;
+});
 
 menuIcon.addEventListener("click", function () {
   menu.style.transform = "TranslateX(0)";
